f44f3c1def5c0b161ca271e167870b1cd436cef0
hemant <hemantyuvasoft114@gmail.com>
2017-10-27 13:25:29 +0530

    feature change production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,23 +11,26 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
+  # config.read_encrypted_secrets = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
+  config.assets.precompile += %w( admin.js admin.css )
 
+ 
  # Do not fallback to assets pipeline if a precompiled asset is missed.
 
 
@@ -35,7 +38,6 @@ Rails.application.configure do
  # yet still be able to expire them through the digest params.
   # config.serve_static_files = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve_static_files = true
   config.assets.compile = true
   config.assets.digest = true
 
@@ -46,7 +48,8 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  #config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
